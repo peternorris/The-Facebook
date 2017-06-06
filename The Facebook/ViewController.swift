2c888bd787612e0c824e26d1102ec5ed794b80af
Peter Norris <peternorris@gmail.com>
2017-06-06 15:02:09 -0400

    Simple interface. Image view and import button. Selecting import button launched an image picker to select an image.

--- The Facebook/ViewController.swift
+++ The Facebook/ViewController.swift
@@ -8,7 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var button: UIButton!
+    
+    @IBAction func lauchImagePicker() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        picker.dismiss(animated: true, completion: nil)
+        
+        self.imageView.image = image
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
