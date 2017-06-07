1c10e02ca22874c94452d2cb2715b089e6bfeb2b
Peter Norris <peternorris@gmail.com>
2017-06-07 14:10:11 -0400

    Finding multiple faces.

--- The Facebook/ViewController.swift
+++ The Facebook/ViewController.swift
@@ -21,8 +21,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var button: UIButton!
     
-    //var boxCoordinates: FaceCube?
-    
     @IBAction func lauchImagePicker() {
         let imagePicker = UIImagePickerController()
         imagePicker.delegate = self
@@ -37,37 +35,32 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         self.imageView.image = image
         
-        //self.findFace()
-        self.findFace() { (faceBox: FaceCube) in
-            self.drawBox(faceBox)
+        self.findFace() { (faceBoxes: [FaceCube]) in
+            self.drawBoxes(faceBoxes)
         }
-        //self.drawBox(self.boxCoordinates)
     }
     
-    func findFace(completionHandler: @escaping (FaceCube) -> Void) {
+    func findFace(completionHandler: @escaping ([FaceCube]) -> Void) {
         if let cgImage = self.imageView.image?.cgImage {
             let requestHandler = VNImageRequestHandler.init(cgImage: cgImage, options:[:])
             let faceRequest = VNDetectFaceRectanglesRequest.init(completionHandler: { (self, error) in
                 if (error != nil) {
                     print("error: \(String(describing: error))")
                 } else {
+                    var faceBoxes: [FaceCube] = []
                     if self.results?.isEmpty == false, let resultsArray = self.results {
-                        let observation = resultsArray.first as! VNFaceObservation
-                        print("observation: \(observation)")
-                        print("observation boundingBox: \(String(describing: observation.boundingBox))")
-                        
-                        let box = observation.boundingBox
-                        
-//                        print("*****")
-//                        print("observation landmark 0 nose points: \(String(describing: observation.landmarks?.nose?.points))")
-//                        print("*****")
-//                        print("observation landmark 0 allPoints: \(String(describing: observation.landmarks?.allPoints))")
-                        
-                        //return FaceCube(x: box.origin.x, y: box.origin.y, width: box.size.width, height: box.size.height)
-                        completionHandler(FaceCube(x: box.origin.x, y: box.origin.y, width: box.size.width, height: box.size.height))
-                        
+                        for tmpObservation in resultsArray {
+                            let observation = tmpObservation as! VNFaceObservation
+                            print("observation: \(observation)")
+                            print("observation boundingBox: \(String(describing: observation.boundingBox))")
+                            
+                            let box = observation.boundingBox
+                            faceBoxes.append(FaceCube(x: box.origin.x, y: box.origin.y, width: box.size.width, height: box.size.height))
+                        }
+                        //completionHandler(FaceCube(x: box.origin.x, y: box.origin.y, width: box.size.width, height: box.size.height))
+                        completionHandler(faceBoxes)
                     } else {
-                        print("No Face Detected")
+                        print("No Faces Detected")
                     }
                 }
             })
@@ -80,8 +73,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         }
     }
     
-    func drawBox(_ coordinates: FaceCube?) {
-        if let coordinates = coordinates {
+    func drawBoxes(_ coordinateBoxes: [FaceCube]) {
+        for coordinates in coordinateBoxes {
             let image = self.imageView.image
             
             let imageSize = image?.size
@@ -97,24 +90,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             
             let rectangle = CGRect(x: x, y: y, width: width, height: height)
             
-            //let heightSpan = floor(height / 2 - rectangle.size.height / 2)
-            //let widthSpan = floor(width / 2 - rectangle.size.width / 2)
-            
             UIColor.green.setFill()
             UIRectFill(rectangle)
-            
-            // fill outer rect
-            //UIColor(red: 0, green: 0, blue: 0, alpha: 0.5).set()
-//            UIRectFill(rectangle)
-            
-            // fill inner border
-            //UIColor(red: 1, green: 1, blue: 1, alpha: 0.5).set()
-//            UIColor.clear.setFill()
-//            UIRectFill(CGRect(x: x + 10, y: y + 10, width: width-20, height: height-20))
-            
-            // fill inner rect
-            //UIColor.clear.set()
-            //UIRectFill(CGRect(x: widthSpan, y: heightSpan, width: rectangle.size.width, height: rectangle.size.height))
             
             let newImage = UIGraphicsGetImageFromCurrentImageContext()
             UIGraphicsEndImageContext()
